8f8d2051eab8b25a9d928bee9bf71f59050d8135
Aidan Vaughan <d00202405@student.dkit.ie>
2019-11-13 11:17:37 +0000

    -Added User Dao

--- MarketingMonitor/src/DAOs/MySqlTestDao.java
+++ MarketingMonitor/src/DAOs/MySqlTestDao.java
@@ -70,4 +70,43 @@ public class MySqlTestDao extends DAOs.MySqlDao implements TestDaoI {
         
     }
     
+    @Override
+    public User Login(int mId) throws DaoException {
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        User u = null;
+        try {
+            con = this.getConnection();
+
+            String query = "SELECT username, password FROM users WHERE username = ?";
+            ps = con.prepareStatement(query);
+            ps.setInt(1, mId);
+
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+                u = new User(username,password);
+            }
+        } catch (SQLException e) {
+            throw new DaoException("Login() " + e.getMessage());
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (ps != null) {
+                    ps.close();
+                }
+                if (con != null) {
+                    freeConnection(con);
+                }
+            } catch (SQLException e) {
+                throw new DaoException("Login() " + e.getMessage());
+            }
+        }
+        return u;     // u may be null 
+    }
+    
 }
--- MarketingMonitor/src/DAOs/TestDaoI.java
+++ MarketingMonitor/src/DAOs/TestDaoI.java
@@ -16,4 +16,5 @@ import java.util.List;
 
 public interface TestDaoI {
     public String testCon() throws DaoException;
+    public User Login() throws DaoException;
 }
